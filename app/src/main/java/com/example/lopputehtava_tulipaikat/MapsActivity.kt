6baf12563548dc988e4c51ca61024da0d6354f3a
villep <pekvil16@gmail.com>
2022-05-03 06:59:29 +0300

    add Kiikelin laavu in Kemi

--- app/src/main/java/com/example/lopputehtava_tulipaikat/MapsActivity.kt
+++ app/src/main/java/com/example/lopputehtava_tulipaikat/MapsActivity.kt
@@ -41,8 +41,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //val sydney = LatLng(-34.0, 151.0)
+        //mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
+        val kemiKiikeli = LatLng(65.74167678944094, 24.54041929313742)
+        mMap.addMarker(MarkerOptions().position(kemiKiikeli).title("Kiikelin laavu, Kemi"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kemiKiikeli))
+        //65.74167678944094, 24.54041929313742
     }
 }
